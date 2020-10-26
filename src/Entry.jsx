c817fbb1c199b183d1fdbab97b65984c80ffcaef
Aiesha Bailey <aiesha.brown.92@gmail.com>
2020-10-26 08:08:18 -0500

    Initial commit

---
+++ src/Entry.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Entry({ emoji, name, description }) {
+  return (
+    <div className="term">
+      <dt>
+        <span className="emoji" role="img" aria-label="Tense Biceps">
+          {emoji}
+        </span>
+        <span>{name}</span>
+      </dt>
+      <dd>{description}</dd>
+    </div>
+  );
+}
+
+export default Entry;
